30c0283857de4c24b38ab5ea456c64028dc14016
Asep Sulaeman <asepsulaeman90@gmail.com>
2024-08-03 20:50:31 +0700

    add secgroup ssh from vpc

--- main.tf
+++ main.tf
@@ -15,13 +15,15 @@ data "aws_ami" "app_ami" {
 }
 
 data "aws_vpc" "default" {
-  default = true
+  default     = true
+  cidr_block  = "172.31.0.0/16"
 }
 
 resource "aws_instance" "web" {
   ami           = "ami-060e277c0d4cce553"
   instance_type = var.instance_type
   vpc_security_group_ids = [aws_security_group.web.id]
+  
 
   tags = {
     Name = "HelloWorld"
@@ -35,7 +37,6 @@ resource "aws_security_group" "web" {
 
   vpc_id = data.aws_vpc.default.id
 }
-
 resource "aws_security_group_rule" "web_http_in" {
   type         = "ingress"
   from_port    = 80
@@ -64,4 +65,22 @@ resource "aws_security_group_rule" "web_everything_out" {
   cidr_blocks  = ["0.0.0.0/0"]
 
   security_group_id = aws_security_group.web.id
+}
+
+
+resource "aws_security_group" "ssh" {
+  name        = "ssh"
+  description = "Allow ssh in"
+
+  vpc_id = data.aws_vpc.default.id
+}
+
+resource "aws_security_group_rule" "web_ssh_in" {
+  type         = "ingress"
+  from_port    = 22
+  to_port      = 22
+  protocol     = "tcp"
+  cidr_blocks  = [data.aws_vpc.default.cidr_block]
+
+  security_group_id = aws_security_group.ssh.id
 }
