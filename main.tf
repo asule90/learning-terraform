721cb0630d53dcc3e52655ee1344ae0c16235ae3
Asep Sulaeman <asepsulaeman90@gmail.com>
2024-08-04 16:47:35 +0700

    fix: vpc subnet

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ module "web_vpc" {
   cidr = "10.0.0.0/16"
 
   azs             = ["ap-southeast-1a", "ap-southeast-1b", "ap-southeast-1c"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  public_subnets  = ["172.31.16.0/20", "172.31.32.0/20", "172.31.0.0/20"]
 
   tags = {
     Terraform = "true"
