0dff448b718723683858c534c73459917447deed
Asep Sulaeman <asepsulaeman90@gmail.com>
2024-08-05 10:39:48 +0700

    using alb

--- main.tf
+++ main.tf
@@ -2,6 +2,13 @@ data "aws_vpc" "default" {
   id = var.vpc_id
 }
 
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
 resource "aws_instance" "web" {
   ami           = var.image_id
   instance_type = var.instance_type
@@ -19,24 +26,76 @@ resource "aws_instance" "web" {
   }
 }
 
-module "web_sg" {
-  source  = "terraform-aws-modules/security-group/aws"
-  version = "5.1.2"
 
-  vpc_id = data.aws_vpc.default.id
-  name    = "web_sg"
+module "web_alb" {
+  source = "terraform-aws-modules/alb/aws"
 
-  ingress_rules = [
-    "http-80-tcp",
-    "https-443-tcp",
-    "ssh-tcp"
-  ]
+  name    = "web-alb"
+  vpc_id  = data.aws_vpc.default.id
 
-  ingress_cidr_blocks = ["0.0.0.0/0"]
+  security_groups = [module.web_sg.security_group_id]
 
-  egress_rules = [
-    "all-all",
-  ]
+  # Security Group
+  security_group_ingress_rules = {
+    all_http = {
+      from_port   = 80
+      to_port     = 80
+      ip_protocol = "tcp"
+      description = "HTTP web traffic"
+      cidr_ipv4   = "0.0.0.0/0"
+    }
+    all_https = {
+      from_port   = 443
+      to_port     = 443
+      ip_protocol = "tcp"
+      description = "HTTPS web traffic"
+      cidr_ipv4   = "0.0.0.0/0"
+    }
+  }
+  security_group_egress_rules = {
+    all = {
+      ip_protocol = "-1"
+      cidr_ipv4   = "0.0.0.0/0"
+    }
+  }
 
-  egress_cidr_blocks = ["0.0.0.0/0"]
+  listeners = {
+    ex-http = {
+      port     = 80
+      protocol = "HTTP"
+
+      forward = {
+        target_group_key = "ex_asg"
+      }
+    }
+    # ex-https = {
+    #   port            = 443
+    #   protocol        = "HTTPS"
+    #   certificate_arn = "arn:aws:iam::123456789012:server-certificate/test_cert-123456789012"
+
+    #   forward = {
+    #     target_group_key = "ex-instance"
+    #   }
+    # }
+  }
+
+  target_groups = { 
+    ex_asg = {
+      backend_protocol        = "HTTP"
+      backend_port            = 80
+      target_type             = "instance"
+      deregistration_delay    = 5
+      target_id               = aws_instance.web.id
+
+      # There's nothing to attach here in this definition.
+      # The attachment happens in the ASG module above
+      # create_attachment     = false
+    }
+
+  }
+
+  tags = {
+    Environment = "Development"
+    Project     = "Example"
+  }
 }
--- security_group.tf
+++ security_group.tf
@@ -2,3 +2,25 @@ resource "aws_key_pair" "deployer" {
   key_name   = "ec2-demo"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDNUXbgqucYmLLkPCI8BVuZeYKSO86Ob+HuqoOqNwCIKpULatfu/4KDvV25eZ/IF1cx0E1qm4Nar/eMYc1HfOdLug6QneTYh5cWbtf4i7dN//Xgf/tQoQYRIiZXda8w9PLdaLZw117c0HUa9DYFR/QK7H7IK0mNsnAtV5nOmwKBcblMD7bvYZtKaVMKV0DZjm+0TCHpPBGiktemJEsFDuOF+qVh0eXE1E0bOT6ZjTm/LY4ksqt55gPvC6KnorPzYagcaCqJs8Oet+gQo1RlPEYt9gccD86FRDBai8VN02RaNDga12j0t+tUojZZRVY/f4X+unDytg9ote+K+wyRI9GVJ0JjJviJVpMy4s9hQriFq+RLjOfKgjVHmizSk++sONNjf0vvnD/COErGKVv77dBO+J2T/PDhB2GaQapOjLqwsLgBuhBE01aFOIAklaZBssrnjda50mil5LqgqEgUTFFiPcvR5Nh4FULpX/4qGd5yFNMWRc0wkXxh9VsFkeFSUitVcklMChhfWhD4bCEfY0FLgsHkwv26WDWlCvLibx4fnQFD9EJOMf2+yUKGLd7OabLgdtc64UICNKDPuBQUmFL+rVxsmW3h3XslTj+bNvP1n04jSLeusw/H6PEH9cRv4haV53OPg4Wd7LxuSaRr2hw+ftB3CQFp8rJZjFGpHlnZ1Q== sule@ABN-325"
 }
+
+module "web_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.1.2"
+
+  vpc_id = data.aws_vpc.default.id
+  name    = "web_sg"
+
+  ingress_rules = [
+    "http-80-tcp",
+    "https-443-tcp",
+    "ssh-tcp"
+  ]
+
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+
+  egress_rules = [
+    "all-all",
+  ]
+
+  egress_cidr_blocks = ["0.0.0.0/0"]
+}
